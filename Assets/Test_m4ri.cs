40df5958894cc08cc970590f4714dc28b9abec8a
xyztematic <larsengel00@gmail.com>
2024-10-05 17:56:01 +0200

    more matrix operations 1

--- Assets/Test_m4ri.cs
+++ Assets/Test_m4ri.cs
@@ -4,6 +4,10 @@ using m4ri;
 
 public class Test_m4ri : MonoBehaviour
 {
+    void Start() {
+        Test();
+        Debug.Log("Testing of m4ri wrapper complete!");
+    }
     public void Test() {
         MatrixGF2 a = new MatrixGF2(1, 50);
         a.Randomize();
@@ -16,6 +20,23 @@ public class Test_m4ri : MonoBehaviour
         MatrixGF2 b = a.Copy();
         b[0, 0] = (~a[0, 0]) & 1; // assigns 0/1 at b[0,0] if a[0,0] is 1/0
         AssertFullInequal(a, b);
+
+        // Simple multiplication
+        MatrixGF2 c = new MatrixGF2(2, 3);
+        c[0,0]=1; c[0,1]=0; c[0,2]=1;
+        c[1,0]=1; c[1,1]=1; c[1,2]=0;
+        MatrixGF2 d = new MatrixGF2(3, 2);
+        d[0,0]=1; d[0,1]=1;
+        d[1,0]=0; d[1,1]=0;
+        d[2,0]=1; d[2,1]=0;
+        MatrixGF2 e = new MatrixGF2(2, 2);
+        e[0,0]=0; e[0,1]=1;
+        e[1,0]=1; e[1,1]=1;
+        AssertFullEqual(c*d, e);
+
+        // Simple addition
+        AssertFullEqual(c+c+c, c);
+        Assert.IsTrue((c+c).IsZero());
     }
 
     public void AssertFullEqual(MatrixGF2 M1, MatrixGF2 M2) {
--- Assets/m4ri/MatrixGF2.cs
+++ Assets/m4ri/MatrixGF2.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text;
+using UnityEngine;
 using static Interface_m4ri;
 
 namespace m4ri {
@@ -13,7 +14,7 @@ namespace m4ri {
             nrows = rows;
             ncols = cols;
         }
-        public MatrixGF2(int rows, int cols, IntPtr ptrToInitializedMatrix) {
+        private MatrixGF2(int rows, int cols, IntPtr ptrToInitializedMatrix) {
             headPtr = ptrToInitializedMatrix;
             nrows = rows;
             ncols = cols;
@@ -22,10 +23,31 @@ namespace m4ri {
             mzd_free(headPtr);
         }
 
+        // Overrides the [] operator on MatrixGF2 instances, allowing reading/writing of an entry with "mat[row, col]"
         public int this[int row, int col] {
             
             get => read_bit(headPtr, row, col);
             set => write_bit(headPtr, row, col, value);
+        }
+        // Overrides the + operator on MatrixGF2 instances, allowing adding two matrices with "mat1 + mat2"
+        public static MatrixGF2 operator+(MatrixGF2 left, MatrixGF2 right) {
+            if (left.nrows == right.nrows && left.ncols == right.ncols) {
+                MatrixGF2 sum = new MatrixGF2(left.nrows, left.ncols);
+                mzd_add(sum.headPtr, left.headPtr, right.headPtr);
+                return sum;
+            }
+            Debug.LogError("Tried to add matrices with different dimensions");
+            return null;
+        }
+        // Overrides the * operator on MatrixGF2 instances, allowing multiplying two matrices with "mat1 * mat2"
+        public static MatrixGF2 operator*(MatrixGF2 left, MatrixGF2 right) {
+            if (left.ncols == right.nrows) {
+                MatrixGF2 prod = new MatrixGF2(left.nrows, right.ncols);
+                mzd_mul_m4rm(prod.headPtr, left.headPtr, right.headPtr);
+                return prod;
+            }
+            Debug.LogError("Tried to multiply matrices with mismatching dimensions");
+            return null;
         }
 
         public override string ToString() {
@@ -39,18 +61,35 @@ namespace m4ri {
             }
             return sb.ToString();
         }
-
+        // Randomizes all entries of the matrix
         public void Randomize() {
             mzd_randomize(headPtr);
         }
+        // Returns a "by value" copy of the matrix
         public MatrixGF2 Copy() {
             return new MatrixGF2(nrows, ncols, mzd_copy(IntPtr.Zero, headPtr));
         }
+        // Checks for equality of two matrices
         public bool IsEqualTo(MatrixGF2 toCompare) {
             return mzd_equal(headPtr, toCompare.headPtr) == 1;
         }
         public static bool AreEqual(MatrixGF2 M1, MatrixGF2 M2) {
             return mzd_equal(M1.headPtr, M2.headPtr) == 1;
+        }
+        // Checks if the matrix is a zero matrix
+        public bool IsZero() {
+            return mzd_is_zero(headPtr) == 1;
+        }
+        public static bool IsZero(MatrixGF2 toCheck) {
+            return mzd_is_zero(toCheck.headPtr) == 1;
+        }
+        // Computes the reduced row echelon form of the matrix "in place" and returns the rank of the matrix
+        public int Rref() {
+            return mzd_echelonize_m4ri(headPtr, 1);
+        }
+        // Computes the NON-REDUCED row echelon form (or upper triangular form) of the matrix "in place" and returns the rank of the matrix
+        public int Ref() {
+            return mzd_echelonize_m4ri(headPtr, 0);
         }
     }
 }
